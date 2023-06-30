71eacf84f1a805665d598debf5fb78e533d329fe
EricJamesCrow <EricCrow@pm.me>
2023-06-29 18:16:52 -0700

    added screenshots

--- models/hash_table.py
+++ models/hash_table.py
@@ -7,13 +7,13 @@ class HashMap:
     def _get_hash(self, key: int) -> int:
         return key - 1
     
-    def resize(self):
+    def resize(self) -> object:
         old_map = self.map
         self.size *= 2
         new_map = [None] * self.size
-        for items in old_map:
-            if items is not None:
-                key, value = items
+        for item in old_map:
+            if item is not None:
+                key, value = item
                 key_hash = self._get_hash(key)
                 key_value = [key, value]
                 new_map[key_hash] = key_value
@@ -21,7 +21,7 @@ class HashMap:
         return self
     
     def insert(self, package_id: int, delivery_address: str, delivery_deadline: str, delivery_city: str, 
-               delivery_zip: str, package_weight: str, delivery_status: str) -> bool:
+               delivery_zip: str, package_weight: str, delivery_status: str) -> object:
         key = package_id
         if key > self.size:
             self.resize()
@@ -32,7 +32,7 @@ class HashMap:
         return self
     
     def update(self, package_id: int, delivery_address: str, delivery_deadline: str, delivery_city: str, 
-            delivery_zip: str, package_weight: str, delivery_status: str) -> list:
+            delivery_zip: str, package_weight: str, delivery_status: str) -> object:
         key = package_id
         value = [delivery_address, delivery_deadline, delivery_city, delivery_zip, package_weight, delivery_status]
         key_hash = self._get_hash(key)
@@ -41,18 +41,18 @@ class HashMap:
         return self
 
     
-    def get(self, key: int) -> list or None:
+    def get(self, key: int) -> list:
         key_hash = self._get_hash(key)
         if self.map[key_hash] is not None:
             return self.map[key_hash][1]
         return None
 
-    def delete(self, key: int) -> bool:
+    def delete(self, key: int) -> object:
         key_hash = self._get_hash(key)
         if self.map[key_hash] is None:
             return False
         self.map[key_hash] = None
-        return True
+        return self
     
     def print(self) -> None:
         print('---PACKAGES----')
